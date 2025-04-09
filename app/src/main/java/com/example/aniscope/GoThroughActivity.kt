a9f4ab81d3ad8a460bc3063eca5bf0472323d406
anshul.mehta <anshul.mehta@bricknbolt.com>
2025-04-10 00:24:26 +0530

    Go Through Screen Added Added Roboto fonts

--- app/src/main/java/com/example/aniscope/GoThroughActivity.kt
+++ app/src/main/java/com/example/aniscope/GoThroughActivity.kt
@@ -2,24 +2,26 @@ package com.example.aniscope
 
 import android.os.Bundle
 import android.util.Log
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.lifecycle.ViewModelProvider
+import com.example.aniscope.databinding.ActivityGoThroughBinding
 import com.example.aniscope.network.Repository
 import com.example.aniscope.view_model.ApiViewModel
 import com.example.aniscope.view_model.ApiViewModelFactory
 
-class MainActivity : AppCompatActivity() {
+class GoThroughActivity : AppCompatActivity() {
 
-    val viewModel by lazy {
+    private val viewModel by lazy {
         ViewModelProvider(this, ApiViewModelFactory(Repository()))[ApiViewModel::class.java]
     }
 
+    var _binding: ActivityGoThroughBinding? = null
+    val binding get() = _binding!!
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        _binding = ActivityGoThroughBinding.inflate(layoutInflater)
+        setContentView(binding.root)
 
         viewModel.getAnimeList(1).observe(this) {
             Log.e("anshul", "onCreate: ${it.body()}", )
